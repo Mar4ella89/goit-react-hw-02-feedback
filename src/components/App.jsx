ac6e5a5c23260f353856763d33e566d78a1529c2
Marat Baikin <3371335@gmail.com>
2023-01-21 14:28:25 +0200

    create components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h2>Please leave feedback</h2>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+
+      <h2>Statistics</h2>
+      <ul>
+        <li>
+          <span>Good</span>
+          <span>0</span>
+        </li>
+        <li>
+          <span>Neutral</span>
+          <span>0</span>
+        </li>
+        <li>
+          <span>Bad</span>
+          <span>0</span>
+        </li>
+        <li>
+          <span>Total</span>
+          <span>0</span>
+        </li>
+        <li>
+          <span>Positive feedback</span>
+          <span>0%</span>
+        </li>
+      </ul>
     </div>
   );
 };
