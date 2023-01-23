74871fc9d0f7249deb4a7913bac63e5d9a260dfe
Marat Baikin <3371335@gmail.com>
2023-01-23 17:29:27 +0200

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -71,64 +71,9 @@ class Section extends Component {
           voits={totalVoits}
           percent={percentPositiveFeedback}
         />
-        {/* <ul>
-          <li>
-            <span>Good:</span>
-            <span>{this.state.good}</span>
-          </li>
-          <li>
-            <span>Neutral:</span>
-            <span>{this.state.neutral}</span>
-          </li>
-          <li>
-            <span>Bad:</span>
-            <span>{this.state.bad}</span>
-          </li>
-          <li>
-            <span>Total:</span>
-            <span>{totalVoits}</span>
-          </li>
-          <li>
-            <span>Positive feedback:</span>
-            <span>{percentPositiveFeedback}%</span>
-          </li>
-        </ul> */}
       </div>
     );
   }
 }
 
 export default Section;
-
-{
-  /* <div>
-      <h2>Please leave feedback</h2>
-      <button>Good</button>
-      <button>Neutral</button>
-      <button>Bad</button>
-
-      <h2>Statistics</h2>
-      <ul>
-        <li>
-          <span>Good</span>
-          <span>0</span>
-        </li>
-        <li>
-          <span>Neutral</span>
-          <span>0</span>
-        </li>
-        <li>
-          <span>Bad</span>
-          <span>0</span>
-        </li>
-        <li>
-          <span>Total</span>
-          <span>0</span>
-        </li>
-        <li>
-          <span>Positive feedback</span>
-          <span>0%</span>
-        </li>
-      </ul>
-    </div> */
-}
